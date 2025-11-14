7274e7abfe75388aa18c3329077728f9aec78322
superspeeder <andy@delusion.org>
2025-11-14 15:23:12 -0800

    convert player controller to use the new unity input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
-{    
+public enum PlayerState {
+    Idle,
+    Jump,
+    Run,
+    Fall,
+}
+
+public class PlayerController : MonoBehaviour {
     //[SerializeField] float moveAcceleration = 10f;
     [SerializeField] float walkSp = 5f;
     [SerializeField] float runJumpVelocity = 5f;
@@ -11,119 +18,139 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float groundedDownVelocity = -0.1f;
     [SerializeField] float gravity = -0.2f;
     [SerializeField] float gravityArcPeak = -0.1f;
+
     [SerializeField] float downGravity = -0.3f;
+
     //number of 60th of a second periods where the player may jump after walking off a platform
     [SerializeField] float coyoteTime = 3.0f;
+
     //number of 60th of a second periods after when the player presses jump where a jump will still register given proper criteria
     [SerializeField] float jumpBufferTime = 3.0f;
     [SerializeField] float gVelScalarIntended = 1.0f;
     [SerializeField] float gVelScalarPrevious = 4.0f;
     [SerializeField] float aVelScalarIntended = 1.0f;
     [SerializeField] float aVelScalarPrevious = 8.0f;
+
     [SerializeField] private Rigidbody2D rb;
+
     //less verbose linearVelocity
     private Vector2 vel;
-    private string state = "idle";
+    private PlayerState state = PlayerState.Idle;
     private bool jumpBuffer = false;
     private bool inCoyoteTime = false;
     private bool didCoyoteTime = false;
 
-    void Start()
-    {
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+    private PlayerInput _playerInput;
+
+    void Start() {
         rb.freezeRotation = true;
-        vel = new Vector2(0,0);
+        vel = new Vector2(0, 0);
+
+        _playerInput = GetComponent<PlayerInput>();
+        _moveAction = _playerInput.actions["Move"];
+        _jumpAction = _playerInput.actions["Jump"];
+
         StartCoroutine(CustomFixedUpdate());
     }
 
-    IEnumerator CoyoteTimeTimer(){
+    IEnumerator CoyoteTimeTimer() {
         inCoyoteTime = true;
         didCoyoteTime = true;
-        yield return new WaitForSeconds(coyoteTime/60);
+        yield return new WaitForSeconds(coyoteTime / 60);
         inCoyoteTime = false;
     }
 
-    IEnumerator JumpBufferTimer(){
+    IEnumerator JumpBufferTimer() {
         jumpBuffer = true;
-        yield return new WaitForSeconds(jumpBufferTime/60);
+        yield return new WaitForSeconds(jumpBufferTime / 60);
         jumpBuffer = false;
     }
 
-    void Update(){
-        if (Input.GetKeyDown(KeyCode.W))
+    void Update() {
+        if (_jumpAction.triggered)
             StartCoroutine(JumpBufferTimer());
     }
 
-    IEnumerator CustomFixedUpdate()
-    {
-        while (true){
+    IEnumerator CustomFixedUpdate() {
+        while (true) {
             vel = rb.linearVelocity;
 
             var temp = Vector2.up * (transform.position.y - 1.05f) + Vector2.right * transform.position.x;
             var isGrounded = Physics2D.Linecast(temp + Vector2.left * 0.3f, temp + Vector2.right * 0.3f);
-            
-            float moveInput = (Input.GetKey(KeyCode.A) ? -1.0f : 0.0f) + (Input.GetKey(KeyCode.D) ? 1.0f : 0.0f);
-            bool kj = Input.GetKey(KeyCode.W);
 
-            switch (state){
-                default:
+            float moveInput = _moveAction.ReadValue<float>();
+            bool kj = _jumpAction.IsPressed();
+
+            switch (state) {
+                case PlayerState.Idle:
                     vel.x /= idleSlowdownScalar;
                     vel.y = groundedDownVelocity;
-                    if (jumpBuffer){
+                    if (jumpBuffer) {
                         vel.y = idleJumpVelocity;
-                        state = "jump";
-                    }else if (moveInput != 0)
-                        state = "run";
+                        state = PlayerState.Jump;
+                    }
+                    else if (moveInput != 0)
+                        state = PlayerState.Run;
                     else if (!didCoyoteTime && !isGrounded)
                         StartCoroutine(CoyoteTimeTimer());
                     else if (!inCoyoteTime)
-                        state = "fall";
-                break;
-                case "run":
-                    vel.x = (moveInput*gVelScalarIntended*walkSp+vel.x*gVelScalarPrevious)/(gVelScalarIntended+gVelScalarPrevious);
+                        state = PlayerState.Fall;
+
+                    break;
+                case PlayerState.Run:
+                    vel.x = (moveInput * gVelScalarIntended * walkSp + vel.x * gVelScalarPrevious) /
+                            (gVelScalarIntended + gVelScalarPrevious);
                     vel.y = groundedDownVelocity;
-                    if (jumpBuffer){
+                    if (jumpBuffer) {
                         vel.y = runJumpVelocity;
-                        state = "jump";
-                    }else if (!didCoyoteTime && !isGrounded)
+                        state = PlayerState.Jump;
+                    }
+                    else if (!didCoyoteTime && !isGrounded)
                         StartCoroutine(CoyoteTimeTimer());
                     else if (moveInput == 0.0f)
-                        state = "idle";
+                        state = PlayerState.Idle;
                     else if (!inCoyoteTime)
-                        state = "fall";
-                break;
-                case "jump":
+                        state = PlayerState.Fall;
+
+                    break;
+                case PlayerState.Jump:
                     jumpBuffer = false;
-                    if (!kj && vel.y > 0){
+                    if (!kj && vel.y > 0) {
                         vel.y *= 0.6f;
                     }
-                    vel.x = (moveInput*aVelScalarIntended*walkSp+vel.x*aVelScalarPrevious)/(gVelScalarIntended+aVelScalarPrevious);
+
+                    vel.x = (moveInput * aVelScalarIntended * walkSp + vel.x * aVelScalarPrevious) /
+                            (gVelScalarIntended + aVelScalarPrevious);
                     vel.y += (vel.y < 1 && kj) ? gravityArcPeak : gravity;
                     if (vel.y < -1)
-                        state = "fall";
+                        state = PlayerState.Fall;
                     else if (moveInput == 0 && isGrounded && vel.y <= 0)
-                        state = "idle";
+                        state = PlayerState.Idle;
                     else if (isGrounded && vel.y <= 0)
-                        state = "run";
-                break;
-                case "fall":
-                    vel.x = (moveInput*aVelScalarIntended*walkSp+vel.x*aVelScalarPrevious)/(gVelScalarIntended+aVelScalarPrevious);
+                        state = PlayerState.Run;
+                    break;
+                case PlayerState.Fall:
+                    vel.x = (moveInput * aVelScalarIntended * walkSp + vel.x * aVelScalarPrevious) /
+                            (gVelScalarIntended + aVelScalarPrevious);
                     vel.y += downGravity;
                     if (moveInput == 0 && isGrounded)
-                        state = "idle";
+                        state = PlayerState.Idle;
                     else if (isGrounded)
-                        state = "run";
-                break;
+                        state = PlayerState.Run;
+                    break;
             }
 
             rb.linearVelocity = vel;
-            yield return new WaitForSeconds(1.0f/60.0f);
+            yield return new WaitForSeconds(1.0f / 60.0f);
         }
     }
 }
 
 /*
 public class PlayerController : MonoBehaviour
-{    
+{
     [SerializeField] float moveAcceleration = 10f;
     [SerializeField] float walkSp = 5f;
     [SerializeField] float jumpForce = 7f;
