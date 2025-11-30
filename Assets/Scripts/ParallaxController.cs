3eeb863270a6ea46c0ecfb0867f426a8932c6dfc
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2025-11-29 20:35:12 -0800

    Improved parallax (I hope)
    Now should track on y-axis

--- Assets/Scripts/ParallaxController.cs
+++ Assets/Scripts/ParallaxController.cs
@@ -4,8 +4,8 @@ public class ParallaxController : MonoBehaviour
 {
 
     private float length, startpos;
-    public GameObject cam;
-    public float parallaxEffect;
+    [SerializeField] public GameObject cam;
+    [SerializeField] public float parallaxEffect;
 
     void Start()
     {
@@ -18,7 +18,7 @@ public class ParallaxController : MonoBehaviour
         float temp = cam.transform.position.x * (1 - parallaxEffect);
         float dist = cam.transform.position.x * parallaxEffect;
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startpos + dist, cam.transform.position.y, transform.position.z);
 
         if (temp > startpos + length) startpos += length;
         else if (temp < startpos - length) startpos -= length;
