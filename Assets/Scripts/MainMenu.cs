3a123b3c3dd59867603a50fd4eb968f393ac0e8b
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2025-12-04 01:17:22 -0800

    Credits dude hell yes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _credits;
+    [SerializeField] private GameObject _creditsButton;
+    [SerializeField] private GameObject _play;
+    [SerializeField] private GameObject _options;
+    [SerializeField] private GameObject _quit;
     public void PlayGame()
     {
         SceneManager.LoadScene("LevelScene");
@@ -11,5 +16,23 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        _credits.SetActive(true);
+        _creditsButton.SetActive(false);
+        _play.SetActive(false);
+        _options.SetActive(false);
+        _quit.SetActive(false);
+    }
+
+    public void Return()
+    {
+        _credits.SetActive(false);
+        _creditsButton.SetActive(true);
+        _play.SetActive(true);
+        _options.SetActive(true);
+        _quit.SetActive(true);
     }
 }
